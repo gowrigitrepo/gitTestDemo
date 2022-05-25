7de84957129fc83fc60a8a7a039c031b97cbd031
gowri <gowri@192.168.0.38>
2022-05-25 18:19:48 -0400

    This is my first commit repo2

---
+++ gitTestDemo/src/gitTestDemo/testGit.java
@@ -0,0 +1,10 @@
+package gitTestDemo;
+
+public class testGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git commit code 1");
+	}
+
+}
